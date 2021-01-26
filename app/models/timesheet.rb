97a0404cf3b84e0f94016340cec269458aebb68a
xDoriana <doriana.negrut@gmail.com>
2021-01-26 13:36:17 +0200

    Update Timesheet create/new option

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -2,6 +2,10 @@ class Timesheet < ApplicationRecord
   belongs_to :employee
   belongs_to :budget
 
+  validate :employee_belongs_to_employer
+  validate :hours_fit_in_a_day
+
+
   validate :hours_are_within_budget_hours
   validate :hours_are_within_budget_hours_left
   validate :date_is_in_budget_date_range
@@ -17,7 +21,7 @@ class Timesheet < ApplicationRecord
 
   def hours_are_within_budget_hours_left
     all_timesheets = Timesheet.where(budget_id: budget_id)
-    #all_timesheets_hours = all_timesheets.sum(&:hours)
+    # aici nu ia toate timesheet-urile, inclusiv asta curenta? sau le ia doar pe alea existente? ca atunci cand fac suma sa compar la if cu orele din buget, mai trebe sa pun si current hours sau nu?
 
     if (hours + all_timesheets.sum(&:hours) > budget.hours) && (hours <= budget.hours)
       errors.add(:hours, "do not fall within budget hours left")
@@ -27,6 +31,22 @@ class Timesheet < ApplicationRecord
   def date_is_in_budget_date_range
     if date_of_service < budget.start_date || date_of_service > budget.end_date
       errors.add(:date_of_service, "does not fall in budget date range")
+    end
+  end
+
+
+
+
+  def employee_belongs_to_employer
+    if employee.employer_id != budget.employer_id
+      # sau daca fac has one through, sa pun != employer.id?
+      errors.add(:employee_id, "and budget do not belong to the same employer")
+    end
+  end
+
+  def hours_fit_in_a_day
+    if hours > 24
+      errors.add(:hours, "do not fit in a day")
     end
   end
 
